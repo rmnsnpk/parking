59552d692459bc05ab7a457da3d63b0a849a9d02
rmnsnpk <snapko0063@gmail.com>
2023-01-30 15:18:57 +0100

    bugfix: provided config service in parking module

--- src/modules/parking/parking.module.ts
+++ src/modules/parking/parking.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ParkingController } from './controllers/parking/parking.controller';
 import { ParkingDataBaseService } from './services/parking/parking-data-base.service';
 import { ParkingService } from './services/parking/parking.service';
 
 @Module({
   controllers: [ParkingController],
-  providers: [ParkingService, ParkingDataBaseService],
+  providers: [ParkingService, ParkingDataBaseService, ConfigService],
 })
 export class ParkingModule {}
